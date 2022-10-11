dea8ca8f5176b558a6107e76b334d02d8dca4a16
awaissaddiqui <awaissaddiqui143@gmail.com>
2022-10-11 10:04:01 -0700

    Lab-1 Activity completed in C++

--- Lab-1/Task-3.cpp
+++ Lab-1/Task-3.cpp
@@ -9,7 +9,42 @@ The program creates an account with an opening balance, displays the balance,
 #include <iostream>
 using namespace std;
 
+class bankAccount{
+	private:
+		int balance;
+	
+	public:
+		bankAccount(){
+			balance = 1000;
+		};
+		void deposit(int payment){
+			balance = balance + payment;
+		}
+		void withdrawal(int amount){
+			if(balance <= 500){
+				cout<<"Insufficient balance";
+			}else{
+				balance = balance - amount ;
+			}
+		};
+		void display(){
+			cout<<"Current Balance = "<<balance<<endl;
+		}
+};
 
 main(){
-	
+	bankAccount obj1;
+	obj1.display();
+	obj1.deposit(500);
+	cout<<"Current Amount = ";
+	obj1.display();
+	obj1.withdrawal(500);
+	cout<<"Current Amount = ";
+	obj1.display();
+	obj1.withdrawal(500);
+	cout<<"Current Amount = ";
+	obj1.display();
+	obj1.withdrawal(500);
+	cout<<"\n";
+	obj1.display();
 }
